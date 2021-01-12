872d8f15f41c55689ee7975229fbfba7577ef52a
Martin Horn <martinhorn55@gmail.com>
2021-01-12 17:32:10 +0200

    Added counters for correct and wrong answers

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
-  const [items, setItems] = useState([])
-  const [count, setCount] = useState(() => { return 1 })
+  const [items, setItems] = useState([]);
+  const [clickable, setClickable] = useState(true);
+  const [count, setCount] = useState(1);
+  const [correct, setCorrect] = useState(0);
+  const [wrong, setWrong] = useState(0);
 
   const BASE_URL = 'https://opentdb.com/api.php?amount=1'
 
@@ -12,35 +15,61 @@ function App() {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(json => setItems(json.results))
-      }, [count])
+  }, [count])
+  
+  useEffect(() => {
+    if ((correct + wrong) === count) {
+      document.getElementsByClassName("correct-ans")[0].style.display = "block";
+      document.getElementsByClassName("next-container")[0].style.display = "block";
+    }
+  })
 
   function nextQuestion() {
-    setCount((count) => count + 1)
+    setCount(count + 1);
+    setClickable(true);
   }
 
-  function showCorrect() {
-    document.getElementsByClassName("correct-ans")[0].style.display = "block";
-    document.getElementsByClassName("next-container")[0].style.display = "block";
+  function sortAnswers() {
+    var answers = items[0].incorrect_answers.concat(items[0].correct_answer);
+
+    for (let i = answers.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [answers[i], answers[j]] = [answers[j], answers[i]];
+    }
+    return answers;
+  }
+  
+  function showCorrect(selected) {
+    setClickable(false);
+    if (items[0].correct_answer === selected) {
+      setCorrect(correct + 1);
+    }
+    else {
+      setWrong(wrong + 1);
+    }
   }
 
   return (
     <>
       {items.map(item => {
-        const answers = item.incorrect_answers.concat(item.correct_answer).sort(() => 0.5 - Math.random())
+        const answers = sortAnswers();
 
-        return <div key={uuidv4()}>
+        return <div className="content" key={uuidv4()}>
           <div className="q-num">Question number {count}</div>
-          <div className="question">{item.question}</div>
+          <div>Correct answers: {correct}</div>
+          <div>Wrong answers: {wrong}</div>
+          <div className="question" dangerouslySetInnerHTML={{__html: item.question}}></div>
 
-          {answers.map(answers => {
-            return <div className="options-div" key={uuidv4()}>
-              <button className="option-btn" onClick={showCorrect} key={uuidv4()}>{answers}</button>
-            </div>
-          })}
-
+          <div className="options-group">
+            {answers.map(option => {
+              return <div className="option-btn" onClick={() => clickable ? showCorrect(option) : null} key={uuidv4()} dangerouslySetInnerHTML={{ __html: option }}></div>
+            })}
+          </div>
+          
           <div className="correct-ans">Correct answer: {item.correct_answer}</div>
+          <div className="correct-ans" dangerouslySetInnerHTML={{ __html: item.correct_answer }}></div>
           <div className="next-container">
-            <button onClick={nextQuestion}>Next Question</button>
+            <div onClick={nextQuestion}>Next Question</div>
           </div>
         </div>
       })}
