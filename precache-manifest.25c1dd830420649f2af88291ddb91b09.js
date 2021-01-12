5c4220333adb51a6de4701c2370af2f694089487
Martin Horn <martinhorn55@gmail.com>
2021-01-12 17:51:05 +0200

    Updates

---
+++ precache-manifest.25c1dd830420649f2af88291ddb91b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966edd9ad52aa7d2e6fe4f6d3c4eade1",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "30db204a62a836d4b081",
+    "url": "/trivia-game/static/css/main.550d27c3.chunk.css"
+  },
+  {
+    "revision": "09ea920a0b52ae249a7b",
+    "url": "/trivia-game/static/js/2.1e7b8ebb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trivia-game/static/js/2.1e7b8ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30db204a62a836d4b081",
+    "url": "/trivia-game/static/js/main.1a0c1e34.chunk.js"
+  },
+  {
+    "revision": "4253b59a7bd63508795a",
+    "url": "/trivia-game/static/js/runtime-main.34912ce3.js"
+  }
+]);
